74d3a774b22dcd7642236c5b212b516b5bcb0335
I Putu Pawesi Siantika <csiantika@gmail.com>
2023-10-02 20:02:01 +0700

    fix thoughput

--- test_throughput.py
+++ test_throughput.py
@@ -1,6 +1,5 @@
 import time
-
-# Define a function that performs an operation (e.g., processing a request)
+import statistics
 
 
 def perform_operation(n):
@@ -10,15 +9,56 @@ def perform_operation(n):
     return result
 
 
-# Measure throughput by counting the number of operations per second
-num_operations = 100_000
-start_time = time.time()
-perform_operation(num_operations)
-end_time = time.time()
-try:
-    elapsed_time = end_time - start_time
-    throughput = num_operations / elapsed_time
-except ZeroDivisionError:
-    print("Elapsed time is near zero.Try increasinf num_operations by tenth!")
-else:
-    print(f"Throughput: {throughput:.2f} operations per second")
+def measure_throughput(operation_function, work_load, num_repeats):
+    throughputs = []  # Store individual throughput values for calculating standard deviation
+    for _ in range(num_repeats):
+        start_time = time.time()
+        operation_function(work_load)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        try:
+            throughput = work_load / elapsed_time
+        except ZeroDivisionError:
+            print(
+                "Elapsed time is near zero. Try increasing num_operations or reducing num_repeats.")
+            return None
+        throughputs.append(throughput)
+
+    average_throughput = sum(throughputs) / num_repeats
+    std_deviation = statistics.stdev(throughputs) if num_repeats > 1 else 0
+
+    return average_throughput, std_deviation
+
+
+if __name__ == "__main__":
+    work_load_1 = 10_000
+    work_load_2 = 100_000
+    work_load_3 = 1_000_000
+    num_repeats = 10
+
+    result_1 = measure_throughput(perform_operation, work_load_1, num_repeats)
+    result_2 = measure_throughput(perform_operation, work_load_2, num_repeats)
+    result_3 = measure_throughput(perform_operation, work_load_3, num_repeats)
+
+    if (result_1 and result_2 and result_3) is not None:
+        average_throughput_1, std_deviation_1 = result_1
+        average_throughput_2, std_deviation_2 = result_2
+        average_throughput_3, std_deviation_3 = result_3
+        print("Workload: 10.000")
+        print(
+            f"Average Throughput: {average_throughput_1:.2f} operations per second")
+        print(
+            f"Standard Deviation: {std_deviation_1:.2f} operations per second (across {num_repeats} runs)")
+        print("\n")
+        print("Workload: 100.000")
+        print(
+            f"Average Throughput: {average_throughput_2:.2f} operations per second")
+        print(
+            f"Standard Deviation: {std_deviation_2:.2f} operations per second (across {num_repeats} runs)")
+        print("\n")
+        print("Workload: 1.000.000")
+        print(
+            f"Average Throughput: {average_throughput_3:.2f} operations per second")
+        print(
+            f"Standard Deviation: {std_deviation_3:.2f} operations per second (across {num_repeats} runs)")
+        print("\n")
